59aa766d26c158bad52393737aa5d8d8165d593d
Vovnenko Oleksii <kespa71for@gmail.com>
2022-07-09 02:06:35 +0300

    create: second task bracket

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const palindrome = str => {
    return str === str.split('').reverse().join('')
 }
 
+
+/*
+   second task brackets
+*/ 
+
+function checkBracket (brackets) {
+   const data = []
+   const getStringBracket = brackets.split('')
+   
+   for (let index = 0; index < getStringBracket.length; index++) {
+      const element = getStringBracket[index];
+         element === '{' ?  data.push(element) :  data.pop(element)
+         data.length === 0 ? console.log('true') : console.log('false')
+   }
+}
+
+checkBracket('{}{}')
